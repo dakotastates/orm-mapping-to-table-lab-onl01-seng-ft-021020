99d1fb3235b7a20826da822b74f6c46315ad227a
Dakota States <dakota.states@gmail.com>
2020-03-12 02:41:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,11 +11,12 @@ attr_reader :id
   end
   
   def self.create_table
-    sql = <<-SQL
+    sql = <<--SQL
     CREATE TABLE IF NOT EXISTS student (
     id INTEGER PRIMARY KEY,
     name TEXT,
-    grade INTEGER)
+    grade INTEGER
+    )
     SQL 
     DB[:conn].exicute(sql)
   end
