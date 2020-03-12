7f8a6ac068f7335cc673a5d828e1526649346e4c
Dakota States <dakota.states@gmail.com>
2020-03-12 03:10:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,5 +33,6 @@ attr_reader :id
     Values (?,?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 end
